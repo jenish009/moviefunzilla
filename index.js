3aea93bbe598ae9d017b515f0126a530e0d83c6c
Jenish <kavathiyajenish009@gmail.com>
2023-09-30 12:18:24 +0530

    add cors

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 require("dotenv").config();
-require('./config/dbConnection')
+require('./config/dbConnection');
+const cors = require("cors"); // Import the cors middleware
 
 const app = express();
 app.use(express.json());
 
+// Use the cors middleware to allow all origins
+app.use(cors());
+
 const { moviesRoutes } = require("./routers");
 app.use("/", moviesRoutes);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000; // Use a default port if PORT is not defined in .env
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
